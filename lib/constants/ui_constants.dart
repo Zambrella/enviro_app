e6f41c0193185277cba73a2dae42e2e0944aa7a1
doug <doug.todd@hey.com>
2021-01-15 21:48:02 +0000

    added theming

---
+++ lib/constants/ui_constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryTextColor = Color(0xff0F2334);
+const Color kSecondaryTextColor = Color(0xfff7D839B);
+const Color kPrimaryColor = Color(0xff3AB29B);
+const Color kBackgroundColor = Colors.white;
+const Color kSecondaryColor = Color(0xff41CD8C);
+const Color kWarningColor = Color(0xffFFA500);
+const Color kErrorColor = Color(0xffB23A51);
+const LinearGradient kPrimaryGradient = const LinearGradient(colors: <Color>[
+  Color(0xff015962),
+  Color(0xff1C946B),
+]);
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:enviro_app/constants/ui_constants.dart';
+
 import 'business_logic/cubits/cubit/intensity_cubit.dart';
 import 'ui/home_page/home_page.dart';
 import 'package:flutter/material.dart';
@@ -11,7 +13,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(),
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        primaryColor: kPrimaryColor,
+        textTheme: TextTheme(
+          headline4: TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 36,
+            fontWeight: FontWeight.w900,
+            color: kPrimaryTextColor,
+          ),
+          headline5: TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 24,
+            fontWeight: FontWeight.w700,
+            color: kPrimaryTextColor,
+          ),
+          headline6: TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 18,
+            fontWeight: FontWeight.w700,
+            color: kPrimaryTextColor,
+          ),
+          subtitle1: TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: kPrimaryTextColor,
+          ),
+          bodyText1: TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            color: kPrimaryTextColor,
+          ),
+          bodyText2: TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            color: kSecondaryTextColor,
+          ),
+          button: TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 14,
+            fontWeight: FontWeight.w700,
+            color: kSecondaryTextColor,
+          ),
+        ),
+      ),
       home: BlocProvider(
         create: (context) => IntensityCubit(),
         child: HomePage(),
